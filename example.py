ab9b25c9cea26f6b113b7a784002e16f61f741a4
Attie Grande <attie@attie.co.uk>
2019-11-27 14:06:11 +0000

    added an example script

---
+++ example.py
@@ -0,0 +1,30 @@
+from time import sleep
+
+from koradctl.port import get_port
+from koradctl.psu import PowerSupply
+
+port = get_port('/dev/ttyACM0')
+psu = PowerSupply(port)
+
+# disable OCP / OVP
+psu.set_ocp_state(False)
+psu.set_ovp_state(False)
+
+# setup output
+psu.set_voltage_setpoint(12)
+psu.set_current_setpoint(0.5)
+
+# enable output
+psu.set_output_state(True)
+
+# wait for startup, and then enable OCP
+sleep(0.25)
+psu.set_ocp_state(True)
+
+# log power usage at ~1Hz
+while True:
+    v = psu.get_output_voltage().value
+    i = psu.get_output_current().value
+    p = psu.get_output_power().value
+    print('%2.2fv    %1.3fA    %2.2fW' % ( v, i, p ))
+    sleep(1)
