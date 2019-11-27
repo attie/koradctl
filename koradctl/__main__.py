0fdec6b034e948ef9c75beb6e587bbe2e39c682c
Attie Grande <attie@attie.co.uk>
2019-11-27 18:27:14 +0000

    moved the cli bootstrap into a function

--- koradctl/__main__.py
+++ koradctl/__main__.py
@@ -3,11 +3,14 @@ from serial.serialutil import SerialException
 
 from koradctl.cli import Cli
 
-if __name__ == '__main__':
+def cli():
     try:
-        cli = Cli()
-        cli.run()
+        cli_app = Cli()
+        cli_app.run()
     except KeyboardInterrupt:
         pass
     except SerialException:
         print('ERROR: The power supply appears to have gone away...', file=sys.stderr)
+
+if __name__ == '__main__':
+    cli()
