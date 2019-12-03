ac5313d15a8aa748f9e09de12aef4a4a9728f58c
Attie Grande <attie@attie.co.uk>
2019-12-03 13:52:07 +0000

    added human_bool_toggle type

--- koradctl/types.py
+++ koradctl/types.py
@@ -1,5 +1,6 @@
 true_values = [ '1', 'on', 'yes' ]
 false_values = [ '0', 'off', 'no' ]
+toggle_values = [ 't', 'toggle' ]
 
 def human_bool(arg: str):
     if arg in true_values:
@@ -8,3 +9,13 @@ def human_bool(arg: str):
         return False
 
     raise TypeError('Must be a "human" boolean (one of: %s)' % ( [ *true_values, *false_values ] ))
+
+def human_bool_toggle(arg: str):
+    if arg in true_values:
+        return True
+    if arg in false_values:
+        return False
+    if arg in toggle_values:
+        return 'toggle'
+
+    raise TypeError('Must be a "human" boolean, or toggle (one of: %s)' % ( [ *true_values, *false_values, *toggle_values ]))
